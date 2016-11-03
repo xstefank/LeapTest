fdcff2c1af28c3f7b2b0fd1d34bf898fb61485e8
xstefank <mjolnir122@gmail.com>
2016-11-03 07:26:05 +0100

    Test scene refactoring, reimport default orion assets

---
+++ Assets/Scripts/GrabObject.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+using Leap.Unity;
+
+public class GrabObject : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //Debug.Log("index --> " + _indexColliding);
+        //Debug.Log("thumb --> " + _thumbColliding);
+    }
+
+    private bool _indexColliding = false;
+    private bool _thumbColliding = false;
+
+    void OnCollisionEnter(Collision other)
+    {
+        Debug.Log(other.gameObject.tag);
+        // keep track of the colliding objects
+        if (other.gameObject.tag == "index")
+            _indexColliding = true;
+        else if (other.gameObject.tag == "thumb")
+            _thumbColliding = true;
+
+        if (_indexColliding && _thumbColliding)
+        {
+            Debug.Log("index thumb colliding");
+            gameObject.transform.Translate(other.transform.position);
+        }
+    }
+
+    void OnCollisionExit(Collision other)
+    {
+        // reset the booleans if the objects are no longer colliding
+        if (other.gameObject.tag == "index")
+            _indexColliding = false;
+        else if (other.gameObject.tag == "thumb")
+            _thumbColliding = false;
+    }
+
+    private HandModel GetHand(Collider other)
+    {
+        if (other.transform.parent && other.transform.parent.parent && other.transform.parent.parent.GetComponent<HandModel>())
+            return other.transform.parent.parent.GetComponent<HandModel>();
+        else
+            return null;
+    }
+
+}
---
+++ Assets/Scripts/HandColliderDebug.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+using Leap.Unity;
+
+public class HandColliderDebug : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        Debug.Log("HandColliderDebug start()");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    private bool IsHand(Collider other)
+    {
+        if (other.transform.parent && other.transform.parent.parent && other.transform.parent.parent.GetComponent<HandModel>())
+            return true;
+        else
+            return false;
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (IsHand(other))
+        {
+            Debug.Log("Yay! A hand collided!");
+        }
+    }
+}
